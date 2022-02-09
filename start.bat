9254cf4a7160790f46ae046c78998216e93bb151
Miraslau Rabikau <31839505+Miroslau@users.noreply.github.com>
2022-02-09 16:27:44 +0300

    chnage link

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 git clone https://github.com/Miroslau/pinktdata-frontend.git -b main
-git clone https://github.com/MonkeyKingBar-bit/pinktada-backend.git -b main
+git clone https://github.com/Miroslau/pinktada-backend.git -b main
